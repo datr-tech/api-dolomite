0e4517d3001f4c95285c8408416357978b98aad6
joealdersonstrachan <joealdersonstrachan@strachan.email>
2025-04-25 22:28:50 +0100

    feat: update dockerfile and db connection

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,19 @@
 import { app } from '@app-ad/api';
-import { apiName, apiPort } from '@app-ad/config';
+import { apiName, apiPort, dbHost, dbName, dbPort } from '@app-ad/config';
 import { logger } from '@datr.tech/leith-common-logger';
+import { db } from '@datr.tech/leith-common-mongodb-connector';
 import 'dotenv/config';
 
 app.listen(apiPort, () => {
   logger.info(`${apiName} listening on ${apiPort}`);
 
   (async () => {
-    //await db.connect();
+    await db.connect({
+      host: dbHost,
+      name: dbName,
+      port: dbPort,
+      user: undefined,
+      pass: undefined,
+    });
   })();
 });
