9ce034be1102848eec75063c0de556981d3a3c28
joealdersonstrachan <joealdersonstrachan@strachan.email>
2025-05-07 13:30:53 +0100

    fix: wrap the call to the db seeder in an 'is connected' condition

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,16 @@
 import { app } from '@app-ad/api';
-import { HopModel, JourneyModel, JourneyTypeModel } from '@app-ad/api/models';
 import { apiName, apiPort, dbHost, dbName, dbPort } from '@app-ad/config';
+import { HopModel, JourneyModel, JourneyTypeModel } from '@app-ad/api/models';
 import { logger } from '@datr.tech/leith-common-logger';
 import { db } from '@datr.tech/leith-common-mongodb-connector';
 import { dolomiteSeeder } from '@datr.tech/leith-common-seeders';
 import 'dotenv/config';
 
 app.listen(apiPort, () => {
-  logger.info(`${apiName} listening on ${apiPort}`);
+  logger.info(`api-${apiName} listening on ${apiPort}`);
 
   (async () => {
-    await db.connect({
+    const stat = await db.connect({
       host: dbHost,
       name: dbName,
       port: dbPort,
@@ -18,6 +18,10 @@ app.listen(apiPort, () => {
       pass: undefined,
     });
 
-    await dolomiteSeeder(HopModel, JourneyModel, JourneyTypeModel);
+    const { isConnected } = stat;
+
+    if (isConnected) {
+      dolomiteSeeder(HopModel, JourneyModel, JourneyTypeModel);
+    }
   })();
 });
